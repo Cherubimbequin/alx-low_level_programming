c67cf8aa4b56ef1605ad11dfd6acb5c359baae9f
root <cherubimbequinetornam@gmail.com>
2023-02-27 14:26:40 -0800

    my main file is kasiye

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,16 @@
 
 
 /**
+
  * reset_to_98n - take a pointer to an int as parameter and updates 
+
  * the value it points to to 98
+
  * @n: point to an int 
+
  * Return: Nothing 
- */
+
+*/
 
 void reset_to_98(int *n);
 {
